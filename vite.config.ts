878fa60dcf0b1abe8546f9f44ab2c97ac4812f9e
ics-nishihara <nishihara@ics-web.jp>
2022-09-27 20:09:13 +0900

    開発環境

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+      },
+    },
+  },
+  server: {
+    port: 3344,
+  },
+});
